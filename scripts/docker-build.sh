f9f75cb882d188a48584d9fec26669a32c7d9c59
blackholegalaxy <blackholegalaxy@users.noreply.github.com>
2020-03-17 12:01:53 -0400

    chore: improve cache on ci (#3)

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
-if [ "$TRAVIS_BRANCH" = "master" ]; then 
-    docker build --no-cache -t "$DOCKERHUB_REGISTRY:latest" .
+docker pull $DOCKERHUB_REGISTRY:latest || true
+
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    docker build --cache-from $DOCKERHUB_REGISTRY:latest -t "$DOCKERHUB_REGISTRY:latest" .
 else
-    docker build --no-cache -t "$DOCKERHUB_REGISTRY:$TRAVIS_BRANCH" .
+    docker build --cache-from $DOCKERHUB_REGISTRY:latest -t "$DOCKERHUB_REGISTRY:$TRAVIS_BRANCH" .
 fi
