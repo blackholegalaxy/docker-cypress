69f11301df7106d809c40a4b0b32720b89518ff2
blackholegalaxy <blackholegalaxy@users.noreply.github.com>
2020-03-20 16:17:17 -0400

    feat: add wait on curl script

--- scripts/wait-on-curl.sh
+++ scripts/wait-on-curl.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 attempt_counter=0
-max_attempts=20
+max_attempts=3
 
-until ping -c 1 "$1"; do
+until $(curl --output /dev/null --silent --head --fail "$1"); do
   if (( attempt_counter == max_attempts )); then
     echo "Max attempts reached"
     exit 1
